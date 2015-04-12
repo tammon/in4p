23e9f06accebbd9f13197cc3eee71773a4e65692
Tammo Schwindt <dev@tammon.de>
2015-04-12 21:11:28 +0200

    added Customer and Product with Mongo Repository

---
+++ src/main/java/de/tammon/dev/in4p/pots/model/Product.java
@@ -0,0 +1,7 @@
+package de.tammon.dev.in4p.pots.model;
+
+/**
+ * Created by tammschw on 12/04/15.
+ */
+public class Product {
+}
---
+++ src/main/java/de/tammon/dev/in4p/pots/model/ProductRepository.java
@@ -0,0 +1,12 @@
+package de.tammon.dev.in4p.pots.model;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by tammschw on 12/04/15.
+ */
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
